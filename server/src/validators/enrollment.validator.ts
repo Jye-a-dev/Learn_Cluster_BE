c1792ae49d6bf1b44de196d251763e4af6c9b3ed
Jye <shokoste1@gmail.com>
2026-02-07 14:05:02 +0700

    change_enrollment_done

--- server/src/validators/enrollment.validator.ts
+++ server/src/validators/enrollment.validator.ts
@@ -2,25 +2,25 @@ import Joi from "joi";
 
 // ===== CREATE ENROLLMENT =====
 export const createEnrollmentSchema = Joi.object({
-  user_id: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).required(),
-  course_id: Joi.number().integer().required(),
+  user_id: Joi.string().guid({ version: ["uuidv1","uuidv4", "uuidv5"] }).required(),
+  course_id: Joi.string().guid({ version: ["uuidv1","uuidv4", "uuidv5"] }).required(),
 });
 
 // ===== UPDATE ENROLLMENT =====
 export const updateEnrollmentSchema = Joi.object({
-  user_id: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).optional(),
-  course_id: Joi.number().integer().optional(),
+  user_id: Joi.string().guid({ version: ["uuidv1","uuidv4", "uuidv5"] }).optional(),
+  course_id: Joi.string().guid({ version: ["uuidv1","uuidv4", "uuidv5"] }).optional(),
 }).min(1);
 
 // ===== GET / DELETE BY ID =====
 export const idParamSchema = Joi.object({
-  id: Joi.number().integer().required(),
+  id: Joi.string().guid({ version: ["uuidv1","uuidv4", "uuidv5"] }).required(),
 });
 
 // ===== QUERY ENROLLMENTS =====
 export const queryEnrollmentsSchema = Joi.object({
   page: Joi.number().integer().min(1).optional(),
   limit: Joi.number().integer().min(1).max(100).optional(),
-  user_id: Joi.string().guid({ version: ["uuidv4", "uuidv5"] }).optional(),
-  course_id: Joi.number().integer().optional(),
+  user_id: Joi.string().guid({ version: ["uuidv1","uuidv4", "uuidv5"] }).optional(),
+  course_id: Joi.string().guid({ version: ["uuidv1","uuidv4", "uuidv5"] }).optional(),
 });
