adf95e10e6fc844cfe39b8798246098389ca029c
Jye <shokoste1@gmail.com>
2026-02-09 22:44:04 +0700

    update participant done

--- server/src/validators/study_date_participant.validator.ts
+++ server/src/validators/study_date_participant.validator.ts
@@ -1,26 +1,47 @@
-// src/validators/study_date_participant.validator.ts
 import Joi from "joi";
 
+/**
+ * Dùng chung cho các ID có khả năng là:
+ * - UUID (hiện tại)
+ * - number (legacy / tương lai)
+ */
+const studyDateIdSchema = Joi.alternatives()
+	.try(
+		Joi.string().guid({
+			version: ["uuidv1", "uuidv4", "uuidv5", "uuidv7"],
+		}),
+		Joi.number().integer().positive(),
+	)
+	.required();
+
+const userIdSchema = Joi.string()
+	.guid({
+		version: ["uuidv1", "uuidv4", "uuidv5", "uuidv7"],
+	})
+	.required();
+
+/* =========================
+   PARAM SCHEMAS
+   ========================= */
+
 export const studyDateIdParamSchema = Joi.object({
-	study_date_id: Joi.number().integer().positive().required(),
+	study_date_id: studyDateIdSchema,
 });
 
 export const userIdParamSchema = Joi.object({
-	user_id: Joi.string()
-		.guid({ version: ["uuidv4", "uuidv5"] })
-		.required(),
+	user_id: userIdSchema,
 });
 
 export const studyDateUserParamSchema = Joi.object({
-	study_date_id: Joi.number().integer().positive().required(),
-	user_id: Joi.string()
-		.guid({ version: ["uuidv4", "uuidv5"] })
-		.required(),
+	study_date_id: studyDateIdSchema,
+	user_id: userIdSchema,
 });
 
+/* =========================
+   BODY SCHEMA
+   ========================= */
+
 export const joinStudyDateSchema = Joi.object({
-	study_date_id: Joi.number().integer().positive().required(),
-	user_id: Joi.string()
-		.guid({ version: ["uuidv4", "uuidv5"] })
-		.required(),
+	study_date_id: studyDateIdSchema,
+	user_id: userIdSchema,
 });
